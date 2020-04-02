217fd7a5679818f544efb4a1cf52f1470d3bcd05
challangerD <59607064+challangerD@users.noreply.github.com>
2020-04-02 22:38:51 +0900

    Update 1search.py

--- 1search.py
+++ 1search.py
@@ -3,9 +3,10 @@ from selenium import webdriver
 driver = webdriver.Chrome('c:/users/chromedriver.exe')
 url = 'http://www.g2b.go.kr/pt/menu/selectSubFrame.do?framesrc=/pt/menu/frameTgong.do?url=http://www.g2b.go.kr:8101/ep/tbid/tbidFwd.do?bidSearchType=1'
 
+driver.get(url)
+time.sleep(5)
+
 def industry ():
-    driver.get(url)
-    time.sleep(5)
-    industry_search = driver.find_element_by_css_selector('btn_finder')
+    industry_search = driver.find_element_by_xpath("//div[@class='tb_inner']/button[@class='btn)finder']")
     industry_search.click()
 #industry()
