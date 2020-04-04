2f8645738191db988bbab08b558e3328c092f1cd
challangerD <59607064+challangerD@users.noreply.github.com>
2020-04-04 22:03:37 +0900

    Update 1search.py

--- 1search.py
+++ 1search.py
@@ -7,7 +7,7 @@ driver.get(url)
 time.sleep(5)
 
 def industry ():
-    industry_search = driver.find_element_by_xpath("//frame[@id='sub']/frame[@name='main']/body/div[@id='content_wrap']/div[@od='container']/div[@class='section']/from[@name='search']/table[@class='table_info']/tbody/div[@class='tb_inner']/button[@class='btn_finder']")
-    industry_search.click()
+    industry_search = driver.find_element_by_xpath("//frame[@id='sub']/frame[@name='main']/body/div[@id='content_wrap']/div[@od='container']/div[@class='section']/from[@name='search']/table[@class='table_info']/tbody/div[@class='tb_inner']/div[@class='tb_inner']/input[@id='industryCd']")
+    industry_search.sendKey('0001')
 
 #industry()
