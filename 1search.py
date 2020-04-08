e436f27cf30d2eccd973ee86b43aa0cdcc9e3a5b
challangerD <59607064+challangerD@users.noreply.github.com>
2020-04-08 22:55:51 +0900

    Update 1search.py

--- 1search.py
+++ 1search.py
@@ -6,12 +6,21 @@ url = 'www.g2b.go.kr:8101/ep/tbid/tbidFwd.do?bidSearchType=1'
 driver.get(url)
 time.sleep(5)
 
-def industry ():
-    industry_search = dirver.find_element_by_id('')
-#industry()
+def industry():
+    i_search = dirver.find_element_by_name('industryCd')
+    i_search.send_key('0001')
 
+def area():
+    a_search = driver.find_element_by_id('area')
+    for option in a_search.find_elements_by_tag_name('option')
+        if option.text == '서울'
+            option.click()
+            break
 
-def area ():
-    area_search = driver.find_element_by_id('area')
-    area_search.select_by_index(11)
+def click_search():
+    Search = driver.find_element_by_xpath('//div[@class="button4"]/a[@class="btn_mdl",contains(text(),"검색")]')
+    Search.click()
+
+industry()
 area()
+click_search()
