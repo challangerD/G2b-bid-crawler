be71f2f15c5e82b54753a5722fb88f357edbcd6d
challangerD <59607064+challangerD@users.noreply.github.com>
2020-04-23 22:49:07 +0900

    Update 1search.py

--- 1search.py
+++ 1search.py
@@ -1,23 +1,17 @@
 from selenium import webdriver
-
+import datetime
+import requests
+from bs4 import BeautifulSoup
 driver = webdriver.Chrome('c:/users/chromedriver.exe')
-url = 'www.g2b.go.kr:8101/ep/tbid/tbidFwd.do?bidSearchType=1'
+
+#검색기간설정,2020%2F04%2F15
+ToD = datetime.today().strftime('%Y%m%d')
+d = datetime.timedelta(days = 3)
+FromD = ToD - d
+
+#토목,조경 공종 코드 입력
+IndstCd = input('토목: 0001, 조경:0005')
+
+url = 'http://www.g2b.go.kr:8101/ep/tbid/tbidList.do?searchType=1&bidSearchType=1&searchDtType=1&fromBidDt={FromD}&toBidDt={ToD}&area=11&industryCd={IndstCd}'
 
 driver.get(url)
-time.sleep(5)
-
-def industry():
-    i_search = dirver.find_element_by_name('industryCd')
-    i_search.send_key('0001')
-
-def area():
-    a = driver.find_element_by_xpath('//*[@id="area"]')
-    a.select_by_value('11')
-
-def click_search():
-    Search = driver.find_element_by_xpath('//*[@id="buttonwrap"]/div/a[1]/span')
-    Search.click()
-
-industry()
-area()
-click_search()
